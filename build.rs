4ccb2fc4ada82479975b958f47db15ef8890e8ac
Jason Couture <jasonc@alertr.info>
2023-01-02 21:41:00 -0500

    Set ramdisk on run

--- build.rs
+++ build.rs
@@ -6,6 +6,7 @@ fn main() {
     // set by cargo, build scripts should use this directory for output files
     let kernel_os_string = std::env::var_os("CARGO_BIN_FILE_KERNEL_kernel").unwrap();
     let kernel_str = kernel_os_string.to_str().unwrap();
+    let ramdisk = Path::new("build.rs").canonicalize().unwrap();
 
     let out_dir_os_string = std::env::var_os("OUT_DIR").unwrap();
     let out_dir_str = out_dir_os_string.to_str().unwrap();
@@ -17,12 +18,14 @@ fn main() {
     // create an UEFI disk image (optional)
     let uefi_path = out_dir.join(UEFI_IMAGE_NAME);
     bootloader::UefiBoot::new(&kernel)
+        .set_ramdisk(&ramdisk)
         .create_disk_image(&uefi_path)
         .unwrap();
 
     // create a BIOS disk image (optional)
     let bios_path = out_dir.join(BIOS_IMAGE_NAME);
     bootloader::BiosBoot::new(&kernel)
+        .set_ramdisk(&ramdisk)
         .create_disk_image(&bios_path)
         .unwrap();
 
