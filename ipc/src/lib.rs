26eb46ce939be6e62ec6d7caa0b539f054866e20
Jason Couture <jasonc@alertr.info>
2023-04-23 16:45:27 -0400

    WIP

--- ipc/src/lib.rs
+++ ipc/src/lib.rs
@@ -1,5 +1,38 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![no_std]
+
+use alloc::collections::BTreeMap;
+extern crate alloc;
+
+
+pub enum DeviceState {
+    Unknown,
+    NoService,
+    Locked,
+    Disabled,
+    Operational
+}
+
+#[repr(C, align(1))]
+#[derive(Clone, Copy, Debug)]
+pub struct DeviceQueryResult{
+    pub id: u64,
+    pub class_id: u64,
+    pub subclass_id: u64,
+    pub device_type_id: u64,
+    pub service_id: u64
+}
+
+pub struct DeviceRegistry {
+    devices: BTreeMap<u64, DeviceQueryResult>   
+}
+
+impl DeviceRegistry {
+    
+}
+
+
+pub trait SystemService {
+    fn control(&mut self, id: u64, command: u64, data: *const u8) -> *const u8;
 }
 
 #[cfg(test)]
