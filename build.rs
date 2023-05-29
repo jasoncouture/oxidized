e1d0cc4b76510abe12b6f9bc3d5dc5d8440a9936
Jason Couture <jasonc@alertr.info>
2023-05-29 16:00:17 -0400

    Remove BIOS support.

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
 use std::path::Path;
 
 const UEFI_IMAGE_NAME: &str = "uefi.img";
-const BIOS_IMAGE_NAME: &str = "bios.img";
 
 fn main() {
     // set by cargo, build scripts should use this directory for output files
@@ -16,16 +15,11 @@ fn main() {
     // https://doc.rust-lang.org/nightly/cargo/reference/unstable.html#artifact-dependencies
     let kernel = Path::new(kernel_str);
 
-    // create an UEFI disk image (optional)
     let uefi_path = out_dir.join(UEFI_IMAGE_NAME);
-    let bios_path = out_dir.join(BIOS_IMAGE_NAME);
     let binding = kernel.to_path_buf();
     let mut disk_image_builder = bootloader::DiskImageBuilder::new(binding);
     disk_image_builder.set_ramdisk(ramdisk);
     disk_image_builder.create_uefi_image(&uefi_path).unwrap();
-    //disk_image_builder.create_bios_image(&bios_path).unwrap();
 
-    // pass the disk image paths as env variables to the `main.rs`
     println!("cargo:rustc-env=UEFI_PATH={}", uefi_path.display());
-    println!("cargo:rustc-env=BIOS_PATH={}", bios_path.display());
 }
--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,17 @@ use std::process::Command;
 fn main() {
     // read env variables that were set in build script
     let uefi_path = env!("UEFI_PATH");
-    let bios_path = env!("BIOS_PATH");
 
-    // choose whether to start the UEFI or BIOS image
-    const UEFI: bool = true;
-    let image = match UEFI {
-        true => uefi_path,
-        false => bios_path,
-    };
-
-    let mut cmd = create_command(image, UEFI);
-    println!("Starting image {} with qemu", image);
+    let mut cmd = create_command(uefi_path);
+    println!("Starting image {} with qemu", uefi_path);
     let mut child = cmd.spawn().expect("Unable to spawn qemu process");
     child.wait().expect("Unable to wait for child exit!");
 }
 
-fn create_command(image_path: &str, uefi: bool) -> Command {
+fn create_command(image_path: &str) -> Command {
     let mut cmd = std::process::Command::new("qemu-system-x86_64");
 
-    if uefi {
-        cmd.arg("-bios").arg(ovmf_prebuilt::ovmf_pure_efi());
-    }
+    cmd.arg("-bios").arg(ovmf_prebuilt::ovmf_pure_efi());
 
     cmd.arg("-drive")
         .arg(format!("format=raw,file={image_path}"))
